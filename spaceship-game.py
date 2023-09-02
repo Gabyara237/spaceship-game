ecbfeb7c8c6c857c6c96e11751278021d82d0872
Gabyara237 <gabyara237@gmail.com>
2023-09-01 17:19:55 -0700

    Add wallpaper, add screen title and add FPS

--- spaceship-game.py
+++ spaceship-game.py
@@ -2,17 +2,24 @@ import pygame
 
 Swidth= 900
 Sheight= 600
+FPS= 60
+
+clock=pygame.time.Clock()
 
 class start():
     pygame.init()
 
-screen= pygame.display.set_mode((Swidth,Sheight))
 
+screen= pygame.display.set_mode((Swidth,Sheight))
+pygame.display.set_caption("My Spaceship Game")
 #
+background = pygame.transform.scale(pygame.image.load('assets/galaxy-background.jpg').convert(),(900,600))
 
 running =True
 
 while running:
+    clock.tick(FPS)
+    screen.blit(background,(0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
